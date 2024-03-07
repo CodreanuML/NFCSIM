cb7d29d0bed124fb54de8da2f264372f2e7f0575
CodreanuML <102348157+CodreanuML@users.noreply.github.com>
2024-03-07 13:59:07 +0200

    Update nfc.py
    Card emulation capabilities added .

--- PN532Emulator/nfc.py
+++ PN532Emulator/nfc.py
@@ -4,12 +4,18 @@ from digitalio import DigitalInOut
 from adafruit_pn532.spi import PN532_SPI
 import time
 
-
+# IMPORRT COMMANDS FOR PCD
 
 from adafruit_pn532.adafruit_pn532 import _COMMAND_INDATAEXCHANGE
 from adafruit_pn532.adafruit_pn532 import _COMMAND_INLISTPASSIVETARGET
 from adafruit_pn532.adafruit_pn532 import _COMMAND_INPSL
 
+#IMPORT COMMANDS FOR PICC
+
+from adafruit_pn532.adafruit_pn532 import _COMMAND_TGINITASTARGET
+from adafruit_pn532.adafruit_pn532 import _COMMAND_TGRESPONSETOINITIATOR
+from adafruit_pn532.adafruit_pn532 import _COMMAND_TGGETDATA
+from adafruit_pn532.adafruit_pn532 import _COMMAND_TGSETDATA
 # singleton class to set up SPI port to communicate with adafruit board
 class NFC_setup():
 	count=0
@@ -32,10 +38,10 @@ class NFC_setup():
 			return cls.cip
 
 
-#board manager - created for lol lvl interaction with the board
-class board_mngr():
+#board manager - created for lor lvl interaction with the board
+class board_mngr_PCD():
 	
-	#create new NFC socket
+	#create new NFC socket,
 	def __init__(self):
 		print("Socket created")
 		self.adafruit_pn532=NFC_setup.initializare()
@@ -82,8 +88,43 @@ class board_mngr():
 	 			return 0
 
 
+	 #function used to config board as a virtual card 
+
+	def config_PICC(self):
+		MODE = [0x05]   # 5 is for PICC only
+		NFC_SENS_RES = [0x04,0x00]
+		NFC_ID=[0x01,0x02,0x03]
+		SEL_RES=[0x20]
+		FELICA_PARAMETERS=[0x01,0xFE,0x05,0x01,0x86,0x04,0x02,0x02,0x03,0x00,0x4B,0x02,0x4F,0x49,0x8A,0x00,0xFF,0xFF]
+		NFCID3t=[0x01,0x01,0x66,0x6D,0x01,0x01,0x10,0x02,0x00,0x00]
+		L_GENERAL_BYTES=[0x00]
+
+		L_HISTORICAL_BYTES=[0x0]
+
+
+		
+
+		connection_response=self.adafruit_pn532.call_function(_COMMAND_TGINITASTARGET,params=MODE+NFC_SENS_RES+NFC_ID+SEL_RES+FELICA_PARAMETERS+NFCID3t+L_GENERAL_BYTES+L_HISTORICAL_BYTES,response_length=255)
+		print("RATS is :"+str(connection_response));
+
+
+
+
+	def PICC_GET_DATA(self):
+		get_data=self.adafruit_pn532.call_function(_COMMAND_TGGETDATA,response_length=255)
+		print(get_data)
+		if get_data :
+			return 1
+		else :
+			return 0
+	
+
+	def PICC_SET_DATA(self , data ) :
+		set_data=self.adafruit_pn532.call_function(_COMMAND_TGSETDATA,params=data,response_length=255)
+
+
 	#function used to activate ISO/DEP ISO14443-4 data exchange speed 
-	# Allowed valies 106 kbps / 212 kbps /424 kbps			
+	# Allowed values 106 kbps / 212 kbps /424 kbps			
 	def configure_PPS (self,val_sp): 			
 		speed=[0x00,0x01,0x02]
 		#speed[0] - >106 kbps
@@ -105,16 +146,40 @@ class board_mngr():
 
 #program entry point 
 
-#Set Up
-board_manager=board_mngr()
+#Set MANAGER - AVAILABLE FOR PICC AND PCD 
+board_manager=board_mngr_PCD()
 print(board_manager.get_firmware_version())
 board_manager.mifare_config()
 
+
+
+
 #loop
 while(True):
-	card_status=board_manager.enlist_target()
+
+	#CONFIG AS PCD
+	#card_status=board_manager.enlist_target()
 	
-	if(card_status):
-		board_manager.configure_PPS(2)
-		board_manager.write_apdu([0x00,0xA4,0x04,0x00,0x10,0xA0,0x00,0x00,0x06,0x04,0x53,0x6D,0x61,0x72,0x74,0x4B,0x65,0x79,0x00,0x01,0x01])
+	#CONFIG AS PICC
+	config_PICC=board_manager.config_PICC()		
+	
+	data=board_manager.PICC_GET_DATA()
+	if data :
+		board_manager.PICC_SET_DATA([0x90,0x00])
+	data=board_manager.PICC_GET_DATA()
+	if data:
+		board_manager.PICC_SET_DATA([0x90,0x00])
+	
+	# SEND APDU
+	#if(card_status):
+		#CONFIG PPS
+		#board_manager.configure_PPS(0)
+
+		#WRITE APDU
+		#board_manager.write_apdu([0x23,0x49])
+		#board_manager.write_apdu([0x00,0xA4,0x04,0x00,0x10,0xA0,0x00,0x00,0x06,0x04,0x53,0x6D,0x61,0x72,0x74,0x4B,0x65,0x79,0x00,0x01,0x01])
 	time.sleep(1)
+
+
+
+#[0x00,0xA4,0x04,0x00,0x10,0xA0,0x00,0x00,0x06,0x04,0x53,0x6D,0x61,0x72,0x74,0x4B,0x65,0x79,0x00,0x01,0x01]
